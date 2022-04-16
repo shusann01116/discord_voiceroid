3b1d6a0fc15971e98a2e5f745909b964f68bd7de
shusann <chansyutaro1116@gmail.com>
2022-04-17 01:52:40 +0900

    Squashed commit of the following:
    commit 8a191e30bc4b12b2855ea4024cb7062e2fe94582
    Author: shusann <chansyutaro1116@gmail.com>
    Date:   Sun Apr 17 01:29:18 2022 +0900
    
        refactor: update for discord.py
    
        Since bot coroutine not woking correctry due to discord.py api got
        update.
        Main function has got refactored with making it into async function.
    
    commit 86c57e93b278a320ec870f3548798280875a54d5
    Author: shusann <chansyutaro1116@gmail.com>
    Date:   Sat Apr 16 17:02:40 2022 +0900
    
        feat: disconnect command
    
    commit b60a918b8a136b266f9647d8920425ab39edf25d
    Author: shusann <chansyutaro1116@gmail.com>
    Date:   Sat Apr 16 16:55:31 2022 +0900
    
        feat: summon akarichan

--- main.py
+++ main.py
@@ -1,59 +1,126 @@
-from discord.flags import Intents
-import pyvcroid2
+import asyncio
+import discord
 from discord.ext import commands
-from pyvcroid2.pyvcroid2 import VcRoid2
+import pyvcroid2
 
 import private
-from bot import VoiceroidTTSBot
 
 
-def main():
-    vc = initializeVoiceRoid()
+class AkariVoice():
+    def __init__(self) -> None:
+        self.vcRoid = pyvcroid2.VcRoid2()
+        self._initVcRoid()
 
-    token = private.token
+    def _initVcRoid(self) -> pyvcroid2.VcRoid2:
+        vcRoidParams = {
+            'volume': 1.0,
+            'speed': 1.0,
+            'pitch': 1.0,
+            'emphasis': 1.0,
+            'pauseMiddle': 80,
+            'pauseLong': 100,
+            'pauseSentence': 20,
+            'masterVolume': 1.0,
+        }
 
-    # to enumerate members field.
-    # see reference: https://stackoverflow.com/questions/64831017/how-do-i-get-the-discord-py-intents-to-work
-    intents = Intents.default()
-    intents.members = True
+        lang_list = self.vcRoid.listLanguages()
+        if "standard" in lang_list:
+            self.vcRoid.loadLanguage("standard")
+        else:
+            raise Exception("No language library")
+        voice_list = self.vcRoid.listVoices()
+        if 0 < len(voice_list):
+            self.vcRoid.loadVoice(voice_list[0])
+        else:
+            raise Exception("No voice library")
+        self.vcRoid.param.volume = vcRoidParams['volume']
+        self.vcRoid.param.speed = vcRoidParams['speed']
+        self.vcRoid.param.pitch = vcRoidParams['pitch']
+        self.vcRoid.param.emphasis = vcRoidParams['emphasis']
+        self.vcRoid.param.pauseMiddle = vcRoidParams['pauseMiddle']
+        self.vcRoid.param.pauseLong = vcRoidParams['pauseLong']
+        self.vcRoid.param.pauseSentence = vcRoidParams['pauseSentence']
+        self.vcRoid.param.masterVolume = vcRoidParams['masterVolume']
 
-    bot = commands.Bot(command_prefix='!', Intents=intents)
+    def generate_voice(self, text, filename="temp.wav") -> str:
+        """Generate wav file and return its name."""
 
-    @bot.event
-    async def on_ready():
-        print("logged in as")
-        print(bot.user.name)
-        print(bot.user.id)
-        print("ready...")
+        speech, _ = self.vcRoid.textToSpeech(text)
 
-    bot.add_cog(VoiceroidTTSBot(bot, vc))
+        with open(filename, "wb") as f:
+            f.write(speech)
 
-    bot.run(token)
+        return filename
 
-def initializeVoiceRoid() -> VcRoid2:
-    # voiceroid2 client
-    vc: VcRoid2 = pyvcroid2.VcRoid2(install_path='F:\AHS\VOICEROID2' ,install_path_x86='F:\AHS\VOICEROID2')
-    lang_list = vc.listLanguages()
-    if "standard" in lang_list:
-        vc.loadLanguage("standard")
-    else:
-        raise Exception("No language library")
 
-    voice_list = vc.listVoices()
-    if 0 < len(voice_list):
-        vc.loadVoice(voice_list[0])
-    else:
-        raise Exception("No voice library")
+akariVoice = AkariVoice()
 
-    vc.param.volume = 1.0
-    vc.param.speed = 1.0
-    vc.param.pitch = 1.0
-    vc.param.emphasis = 1.0
-    vc.param.pauseMiddle = 80
-    vc.param.pauseLong = 100
-    vc.param.pauseSentence = 200
-    vc.param.masterVolume = 1.0
-    return vc
 
-if __name__ == "__main__":
-    main()
+class Akari(commands.Cog):
+    def __init__(self, bot) -> None:
+        self.bot = bot
+        self.channel: discord.channel = None
+        self.voice_client: discord.VoiceClient = None
+
+    @commands.Cog.listener()
+    async def on_message(self, msg: discord.Message):
+        """Is called when messages are posted"""
+        # Do nothing when not connected
+        if self.channel is None:
+            return
+
+        # Do nothing with command prefix
+        if msg.content.startswith('!'):
+            return
+
+        voice = akariVoice.generate_voice(msg.content)
+        source = discord.FFmpegPCMAudio(voice)
+
+        # Wait Akari chan until finish speaking
+        while self.voice_client.is_playing():
+            await asyncio.sleep(0.1)
+
+        self.voice_client.play(source)
+
+    @commands.command()
+    async def akari(self, ctx: commands.Context):
+        """Joins a voice channel"""
+
+        self.channel = ctx.author.voice.channel
+
+        if ctx.voice_client is not None:
+            return await ctx.voice_client.move_to(self.channel)
+
+        self.voice_client = await self.channel.connect()
+
+    @commands.command()
+    async def bye(self, ctx: commands.Context):
+        """Disconnects the bot from voice"""
+
+        self.channel = None
+        self.voice_client = None
+        await ctx.voice_client.disconnect()
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(
+    command_prefix=commands.when_mentioned_or("!"),
+    description='Akarichan dayo',
+    intents=intents,
+)
+
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user} (ID: {bot.user.id})')
+    print('-----------')
+
+
+async def main():
+    async with bot:
+        await bot.add_cog(Akari(bot))
+        await bot.start(private.token)
+
+asyncio.run(main())
